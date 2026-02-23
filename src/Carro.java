4377d1cefa47ee9917bb565b2a0a666786eea4e5
maicon <ladnim2003@gmail.com>
2026-02-23 00:18:32 -0300

    chore: initial commit

---
+++ src/Carro.java
@@ -0,0 +1,18 @@
+public class Carro
+{
+    public String modelo;
+    public String marca;
+    public String cor;
+    public int ano;
+
+
+    public Carro(String modelo, String marca, String cor, int ano)
+    {
+         this.modelo = modelo;
+        this.marca = marca;
+        this.cor = cor;
+        this.ano = ano;
+
+    }
+
+}
---
+++ src/main.java
@@ -0,0 +1,13 @@
+public class main
+{
+    public static void main(String[] args)
+    {
+        Carro fusca = new Carro("fusca","volkswagen","verde",1997);
+        System.out.println(fusca.ano);
+        System.out.println(fusca.modelo);
+        System.out.println(fusca.marca);
+
+
+
+    }
+}
