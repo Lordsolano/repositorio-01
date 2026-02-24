db3a534e1df17beda0421f6c76d85125c2614079
maicon <ladnim2003@gmail.com>
2026-02-24 20:30:56 -0300

    aula funcionalidades

--- src/Carro.java
+++ src/Carro.java
@@ -1,18 +1,47 @@
-public class Carro
-{
+public class Carro {
     public String modelo;
     public String marca;
     public String cor;
     public int ano;
+    public boolean ligado;
+    public int aceleracao;
 
 
-    public Carro(String modelo, String marca, String cor, int ano)
-    {
-         this.modelo = modelo;
+    public Carro(String modelo, String marca, String cor, int ano) {
+        this.modelo = modelo;
         this.marca = marca;
         this.cor = cor;
         this.ano = ano;
+        this.ligado = false;
+        this.aceleracao = 0;
 
     }
 
+    public void ligar() {
+        if (this.ligado == true) {
+            return;
+        }
+        this.ligado = true;
+    }
+
+    public void desligar() {
+        if (!this.ligado){
+            return;
+        }
+        this.aceleracao = 0;
+        this.ligado = false;
+    }
+    public void acelerar(String tipoaceleracao)
+    {
+        switch (tipoaceleracao) {
+            case "forte":
+                this.aceleracao += 1000;
+                break;
+            case "fraca":
+                this.aceleracao += 500;
+                break;
+            default:
+                this.aceleracao += 750;
+        }
+    }
 }
--- src/main.java
+++ src/main.java
@@ -3,11 +3,10 @@ public class main
     public static void main(String[] args)
     {
         Carro fusca = new Carro("fusca","volkswagen","verde",1997);
-        System.out.println(fusca.ano);
-        System.out.println(fusca.modelo);
-        System.out.println(fusca.marca);
+        fusca.ligar();
+        fusca.acelerar("forte");
 
-
-
+        System.out.println(fusca.aceleracao);
+        System.out.println(fusca.ligado);
     }
 }
